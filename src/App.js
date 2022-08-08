cddb2c8ce5690327c345bfc6cd0bccbee9b8109c
mangeshipatil <thisismangeshi@gmail.com>
2022-08-08 17:44:52 +0530

    Styled buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <div className='container '>
+        <div className='drum-container'>
+          <div className='col-md-6'>
+            <div className='hori-1'>
+              <div className='box-1 box'>Q</div>
+              <div className='box-2 box'>W</div>
+              <div className='box-3 box'>E</div>
+            </div>
+            <div className='hori-2'>
+              <div className='box-1 box'>A</div>
+              <div className='box-2 box'>S</div>
+              <div className='box-3 box'>D</div>
+            </div>
+            <div className='hori-3'>
+              <div className='box-1 box'>Z</div>
+              <div className='box-2 box'>X</div>
+              <div className='box-3 box'>C</div>
+            </div>
+          </div>
+          <div className='col-md-6'>hwwww</div>
+        </div>
+      </div>
     </div>
   );
 }
