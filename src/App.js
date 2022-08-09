26a3da8169d2352b6636b83de645bee5bd0d7a62
mangeshipatil <thisismangeshi@gmail.com>
2022-08-09 11:42:54 +0530

    Added sound to buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import data from "./JSON"
 import './App.css';
 
 function App() {
@@ -8,19 +8,19 @@ function App() {
         <div className='drum-container'>
           <div className='col-md-6'>
             <div className='hori-1'>
-              <div className='box-1 box'>Q</div>
-              <div className='box-2 box'>W</div>
-              <div className='box-3 box'>E</div>
-            </div>
-            <div className='hori-2'>
-              <div className='box-1 box'>A</div>
-              <div className='box-2 box'>S</div>
-              <div className='box-3 box'>D</div>
-            </div>
-            <div className='hori-3'>
-              <div className='box-1 box'>Z</div>
-              <div className='box-2 box'>X</div>
-              <div className='box-3 box'>C</div>
+
+              {data.map((e,i) => {
+                const audio = new Audio(e.url);
+                return (
+                  <>
+                    <div key={i} className="hori-1">
+                      <input key={i} className="box" type="button" value={e.aplhabet} onClick={()=>audio.play()} />
+                    </div>
+
+                  </>
+                )
+
+              })}
             </div>
           </div>
           <div className='col-md-6'>hwwww</div>
@@ -31,3 +31,5 @@ function App() {
 }
 
 export default App;
+
+
---
+++ src/JSON.js
@@ -0,0 +1,51 @@
+const data = [{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+    aplhabet:'Q'
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+    aplhabet:'W'
+
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+    aplhabet:'E'
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+    aplhabet:'A'
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+    aplhabet:'S'
+
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+    aplhabet:'D'
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+    aplhabet:'Z'
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+    aplhabet:'X'
+
+
+},
+{
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    aplhabet:'C'
+
+}
+]
+
+export default data
